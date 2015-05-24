a08612e925652261d6e59409214b2ff2701f6c59
Liupeng <aliupeng@yahoo.com>
2015-05-24 16:00:04 +0800

    register

---
+++ Register.cpp
@@ -0,0 +1,18 @@
+/*
+ * Register.cpp
+ *
+ *  Created on: 2015Äê5ÔÂ24ÈÕ
+ *      Author: Andy
+ */
+
+#include "Register.h"
+
+Register::Register() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Register::~Register() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Register.h
@@ -0,0 +1,17 @@
+/*
+ * Register.h
+ *
+ *  Created on: 2015Äê5ÔÂ24ÈÕ
+ *      Author: Andy
+ */
+
+#ifndef REGISTER_H_
+#define REGISTER_H_
+
+class Register {
+public:
+	Register();
+	virtual ~Register();
+};
+
+#endif /* REGISTER_H_ */
