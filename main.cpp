5faa9bb7a36ccc32f5ad681d2f42a6dcc77d51a9
chenzero <stzdzyhs@gmail.com>
2015-05-24 13:21:20 +0800

    add a test main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>  
+#include <stdio.h>
+#include <stdlib.h>
+#include "tcpclient.h"
+  
+using namespace std;  
+
+
+  
+int main(int argc,char* argv[])  
+{  
+		unsigned char com[9] ={0x7e, 0x30, 0x7d,0x02,0x08,0x7d,0x01,0x55,0x7e};
+		int len =0;
+		unsigned char original[11];
+        tcp_client tc;  
+		tc.toOriginalMsg(com, 9,  original,&len );
+		for(int i = 0; i<len; i++) {
+			printf("%x ",original[i]);
+		}
+        printf("\n");
+        printf("%d\n",len);
+        printf("\n");
+
+		toOriginalMsg2(com, 9,  original,&len );
+		for(int i = 0; i<len; i++) {
+			printf("%x ",original[i]);
+		}
+        printf("\n");
+        printf("%d\n",len);
+        return 0;  
+}  
+
